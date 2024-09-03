e0ee5853d16312e1c343af2118fcd54185374e10
Manan Gandhi <ardumanan@gmail.com>
2024-09-03 22:08:39 +0530

    Base folder structure and mongoose schema

---
+++ handlers/auth.js
@@ -0,0 +1,5 @@
+const registerHandler = async (req, res) => {};
+const loginHandler = async (req, res) => {};
+const userDataHandler = async (req, res) => {};
+
+export { registerHandler, loginHandler, userDataHandler };
