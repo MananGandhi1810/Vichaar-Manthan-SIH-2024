dda04e1836c3b9be374bf6f9160f1540f9a40a4e
Manan Gandhi <ardumanan@gmail.com>
2024-09-04 10:49:04 +0530

    Added Kafka utilities

---
+++ utils/queue-manager.js
@@ -0,0 +1,21 @@
+import { Kafka } from "kafkajs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const kafka = new Kafka({
+    clientId: "express-backend",
+    brokers: [process.env.KAFKA_BROKER ?? "localhost:9092"],
+});
+
+const producer = kafka.producer();
+await producer.connect();
+
+const send = async (topic, message) => {
+    await producer.send({
+        topic,
+        messages: [{ value: message }],
+    });
+};
+
+export { send };
