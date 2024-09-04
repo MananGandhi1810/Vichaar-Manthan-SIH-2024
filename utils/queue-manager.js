087139071269e5d50feadddf4af29f7be7a81a2a
Manan Gandhi <ardumanan@gmail.com>
2024-09-04 12:19:25 +0530

    Better Kafka Error Handling

--- utils/queue-manager.js
+++ utils/queue-manager.js
@@ -2,20 +2,35 @@ import { Kafka } from "kafkajs";
 import dotenv from "dotenv";
 
 dotenv.config();
+var isConnected = false;
 
-const kafka = new Kafka({
-    clientId: "express-backend",
-    brokers: [process.env.KAFKA_BROKER ?? "localhost:9092"],
-});
-
-const producer = kafka.producer();
-await producer.connect();
-
-const sendQueueMessage = async (topic, message) => {
-    await producer.send({
-        topic,
-        messages: [{ value: message }],
+try {
+    const kafka = new Kafka({
+        clientId: "express-backend",
+        brokers: [process.env.KAFKA_BROKER ?? "localhost:9092"],
     });
+
+    const producer = kafka.producer();
+    await producer.connect();
+    isConnected = true;
+} catch (e) {
+    console.log("Couldn't connect to Kafka Broker as a producer");
+    console.log(e);
+}
+const sendQueueMessage = async (topic, message) => {
+    if (!isConnected) {
+        console.log("Couldn't send message to kafka broker");
+        return;
+    }
+    try {
+        await producer.send({
+            topic,
+            messages: [{ value: message }],
+        });
+    } catch (e) {
+        console.log("Couldn't send message to kafka broker");
+        console.log(e);
+    }
 };
 
 export { sendQueueMessage };
