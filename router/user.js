bd8709bbaf92f6f49fce045e4ab806e993f6f573
Manan Gandhi <ardumanan@gmail.com>
2024-09-05 15:02:15 +0530

    Added placeholder for answer upload

--- router/user.js
+++ router/user.js
@@ -14,5 +14,6 @@ userRouter.post("/resume/:role", checkAuth, uploadResumeHandler);
 userRouter.get("/resume/:role", checkAuth, getResumeHandler);
 userRouter.get("/resume/:role/:id", checkAuth, getResumeWithIdHandler);
 userRouter.get("/questions/:role/:id", checkAuth, getQuestionsHandler);
+userRouter.post("/questions/:role/:id/:index", checkAuth, (){});
 
 export default userRouter;
