e3faa933f0f96fcc0a8e4637c8ad4a47259938df
ItsSpirax <60130987+ItsSpirax@users.noreply.github.com>
2024-09-05 21:39:03 +0530

    Undo

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 
 dotenv.config();
 const jwtSecret = process.env.JWT_SECRET;
-console.log(jwtSecret);
 
 const checkAuth = async (req, res, next) => {
     const { authorization } = req.headers;
