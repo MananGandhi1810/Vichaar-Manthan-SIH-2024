189ed1be1429cdcc1a83893e2ff14aa636fecbde
Manan Gandhi <ardumanan@gmail.com>
2024-09-07 13:40:25 +0530

    Formatted Code

--- express/router/user.js
+++ express/router/user.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import {
-    getResumeHandler,
-    getResumeWithIdHandler,
-    getQuestionsHandler,
-    uploadResumeHandler,
-    setAnswerHandler,
-    getFeedbackHandler,
+  getResumeHandler,
+  getResumeWithIdHandler,
+  getQuestionsHandler,
+  uploadResumeHandler,
+  setAnswerHandler,
+  getFeedbackHandler,
 } from "../handlers/user.js";
 
 import { checkAuth } from "../middlewares/auth.js";
@@ -17,6 +17,10 @@ userRouter.get("/resume/:role", checkAuth, getResumeHandler);
 userRouter.get("/resume/:role/:id", checkAuth, getResumeWithIdHandler);
 userRouter.get("/questions/:role/:id", checkAuth, getQuestionsHandler);
 userRouter.post("/questions/:role/:id/:index", checkAuth, setAnswerHandler);
-userRouter.get("/questions/:role/:id/getFeedback", checkAuth, getFeedbackHandler);
+userRouter.get(
+  "/questions/:role/:id/getFeedback",
+  checkAuth,
+  getFeedbackHandler,
+);
 
 export default userRouter;
